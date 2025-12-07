4f08e5a637a6582500ba46e24504424feb0e3154
alimirza093 <ali8751805@gmail.com>
2025-12-07 19:02:38 +0500

    Change Deploy Config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://shop-management-web-app.vercel.app";
 
 export async function checkPassword() {
   const res = await fetch(`${API_URL}/check_password`, {
